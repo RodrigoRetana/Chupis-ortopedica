3300c22853b2006375c6c0251ed72d031eb31893
Rodrigo <rretanal@ucenfotec.ac.cr>
2026-06-07 11:36:26 -0600

    Remove legacy domain entities and controllers
    Delete legacy domain model classes, repositories and REST controllers for game, gift, giftList, movie, preferenceList, team and player (entities and repositories under src/main/java/com/project/demo/logic/entity/* and controllers under src/main/java/com/project/demo/rest/*). Also update .gradle/file-system.probe. This cleans up unused/obsolete code in preparation for a refactor or replacement of these modules.

--- src/main/java/com/project/demo/logic/entity/giftList/GiftListRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.demo.logic.entity.giftList;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GiftListRepository extends JpaRepository<GiftList, Long> {
-}
